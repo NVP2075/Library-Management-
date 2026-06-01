db6819aa7cb806683fdb54b7402c6f42b364fc71
NguyenVanPhap <vanphapnguyen79@gmail.com>
2026-06-01 17:16:16 +0700

    Hoan thanh project

--- src/main/java/HibernateUtil/OverdueCheckTask.java
+++ src/main/java/HibernateUtil/OverdueCheckTask.java
@@ -9,20 +9,29 @@ public class OverdueCheckTask implements Runnable {
     public void run() {
         EntityManager em = Hibernate.getEmf().createEntityManager(); 
         try {
-            em.getTransaction().begin();
-      
-            String hql = "UPDATE BorrowTicketDetail b SET b.status = 'OVERDUE' " +
-                         "WHERE b.status = 'ACTIVE' AND b.due_date < :now AND b.return_date IS NULL";
-            
-            int updatedCount = em.createQuery(hql)
-                                 .setParameter("now", new Date())
-                                 .executeUpdate();
-            
-            em.getTransaction().commit();
-            
-            if (updatedCount > 0) {
-                System.out.println("[Scheduler] Đã tự động cập nhật " + updatedCount + " phiếu mượn sang trạng thái Quá Hạn.");
-            }
+        	em.getTransaction().begin();
+
+        	Date now = new Date();
+
+        	
+        	String updateDetailHql = "UPDATE BorrowTicketDetail d SET d.status = 'OVERDUE' " +
+        	                         "WHERE d.status = 'BORROWED' AND d.due_date < :now AND d.return_date IS NULL";
+        	int detailCount = em.createQuery(updateDetailHql)
+        	                    .setParameter("now", now)
+        	                    .executeUpdate();
+
+        	
+        	String updateTicketHql = "UPDATE BorrowTicket t SET t.status = 'OVERDUE' " +
+        	                         "WHERE t.status = 'ACTIVE' AND t.id IN " +
+        	                         "(SELECT d.ticket_id.id FROM BorrowTicketDetail d WHERE d.status = 'OVERDUE')";
+        	int ticketCount = em.createQuery(updateTicketHql)
+        	                    .executeUpdate();
+
+        	em.getTransaction().commit();
+
+        	if (detailCount > 0 || ticketCount > 0) {
+        	    System.out.println("[Scheduler] Đã tự động cập nhật " + detailCount + " cuốn sách và " + ticketCount + " phiếu mượn sang Quá Hạn.");
+        	}
         } catch (Exception e) {
             em.getTransaction().rollback();
             e.printStackTrace();
